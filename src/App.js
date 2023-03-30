8decba14e50bcf1e10a924994a8accc2a434321a
JCiaffa <jciaffa@zagmail.gonzaga.edu>
2023-03-29 22:41:55 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Visualizer from "./components/Visualizer";
+
+function App() {
+  return (
+    <div className="App">
+      <Visualizer></Visualizer>
+    </div>
+  );
+}
+
+export default App;
